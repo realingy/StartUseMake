86a9ef68d61aefa5a0696de6056e2df4453ac2ea
realingy <754037927@qq.com>
2019-04-15 13:56:50 +0800

    [changelog] add make demo3

---
+++ GNUMakeDemo3/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "global.h"
+using namespace std;
+
+void test1();
+void test2();
+void test3();
+
+int main()
+{
+    cout<<TEXT<<" main"<<endl;
+    test1();
+    test2();
+    test3();
+
+    return 0;
+}
---
+++ GNUMakeDemo3/test2.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file test2.cpp
+ * @brief  
+ * @author  
+ * @version 1.0
+ * @date 2018年04月15日 13时01分17秒
+ */
+
+#include <iostream>
+#include "global.h"
+
+void test2()
+{
+    std::cout<<TEXT<<" 2"<<std::endl;
+}
+
